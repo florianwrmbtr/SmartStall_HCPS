3060a42710db5c0a344e2c91bf6ec4a2045fc037
hnnsbrhn <hannes_bruhn@web.de>
2023-06-15 14:23:41 +0200

    add: model for received data add: service to write received data in sql database

--- Test2/Controllers/ReceiveDataController.cs
+++ Test2/Controllers/ReceiveDataController.cs
@@ -6,13 +6,14 @@ using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 using Test2.Models;
+using Test2.Services;
 
 namespace Test2.Controllers
 {
     public class ReceiveDataController : Controller
     {
         [HttpPost]
-        public ActionResult ReceiveData(SensorData data)
+        public ActionResult ReceiveData(Datenpaket data)
         {
             if (data != null)
             {
@@ -20,9 +21,13 @@ namespace Test2.Controllers
                 // Zum Beispiel: Daten in einer Datenbank speichern
                 // oder weitere Aktionen durchführen
 
-                // Beispiel: Daten in der Konsole ausgeben
-                string json = JsonConvert.SerializeObject(data);
-                System.Console.WriteLine($"Received sensor data: {json}");
+                // Beispiel: Daten in der Konsole ausgeben                
+                System.Console.WriteLine($"Received sensor data: {JsonConvert.SerializeObject(data)}");
+
+                DbWriteService writeService = new DbWriteService();
+                writeService.InsertData(data);
+
+
 
                 // Rückgabe einer erfolgreichen Antwort
                 return Content("Data received successfully");
@@ -31,13 +36,5 @@ namespace Test2.Controllers
             // Falls keine Daten empfangen wurden, eine Fehlerantwort zurückgeben
             return Content("No data received");
         }
-    }
-
-    public class SensorData
-    {
-        public float Temperature { get; set; }
-        public float Pressure { get; set; }
-        public float Humidity { get; set; }
-        public float GasResistance { get; set; }
     }
 }
