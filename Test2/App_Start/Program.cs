f74a75b1b6d95743f4ce12d60328f7675768e2f5
hnnsbrhn <hannes_bruhn@web.de>
2023-06-15 19:52:11 +0200

    add: main method

---
+++ Test2/App_Start/Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Hosting;
+
+namespace Test2
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Entry point logic
+        }
+    }
+}
